fd495593af0bd5ba7af93a9aaf6b6c7bc0f34b66
Ronaldo <ronaldomuniz@gmail.com>
2020-04-30 17:09:56 +1200

    Update last capabilities of last version of Nova.

--- resources/js/components/DefaultField.vue
+++ resources/js/components/DefaultField.vue
@@ -1,7 +1,7 @@
 <template>
 
     <div ref="field-wrapper" :class="this.field.size"> <!-- ADD this.field.size to this div -->
-        <field-wrapper>
+        <field-wrapper :stacked="field.stacked">
             <div :class="fieldWrapperClasses">
 
                 <div :class="fieldLabelClasses">
@@ -11,17 +11,26 @@
                             :class="{ 'mb-2': showHelpText && field.helpText }"
                         >
                             {{ fieldLabel }}
+
+                            <span v-if="field.required" class="text-danger text-sm">{{
+            __('*')
+          }}</span>
                         </form-label>
                     </slot>
                 </div>
                 <div :class="fieldClasses">
                     <slot name="field" />
 
-                    <help-text class="error-text mt-2 text-danger" v-if="showErrors && hasError">
+                    <help-text
+                            class="error-text mt-2 text-danger"
+                            v-if="showErrors && hasError"
+                    >
                         {{ firstError }}
                     </help-text>
 
-                    <help-text class="help-text mt-2" v-if="showHelpText"> {{ field.helpText }} </help-text>
+                    <help-text class="help-text mt-2" v-if="showHelpText">
+                        {{ field.helpText }}
+                    </help-text>
                 </div>
 
             </div>
@@ -57,14 +66,32 @@
         },
 
         computed: {
+            /**
+             * Return the label that should be used for the field.
+             */
             fieldLabel() {
-                // If the field name is purposefully an empty string, then
-                // let's show it as such
+                // If the field name is purposefully an empty string, then let's show it as such
                 if (this.fieldName === '') {
                     return ''
                 }
 
-                return this.fieldName || this.field.singularLabel || this.field.name
+                return this.fieldName || this.field.name || this.field.singularLabel
+            },
+
+            /**
+             * Return the classes that should be used for the field content.
+             */
+            fieldClasses() {
+
+                if(this.hasSize) {
+                    return 'w-full';
+                }
+
+                return 'py-6 px-8 ' + (this.fullWidthContent
+                    ? this.field.stacked
+                        ? 'w-full'
+                        : 'w-4/5'
+                    : 'w-1/2')
             },
 
             hasSize() {
@@ -72,20 +99,12 @@
             },
 
             fieldLabelClasses() {
-                return this.hasSize ? 'nova-grid-field-label' : 'w-1/5 py-6 px-8';
+                return this.hasSize ? 'nova-grid-field-label' :
+                    ('w-1/5 px-8' + this.field.stacked ? ' pt-6' : ' py-6');
             },
 
             fieldWrapperClasses() {
                 return this.field.size !== undefined ? 'nova-grid-wrapper' : 'flex border-b border-40 w-full';
-            },
-
-            fieldClasses() {
-
-                if(this.hasSize) {
-                    return 'w-full';
-                }
-
-                return this.fullWidthContent ? 'py-6 px-8 w-4/5' : 'py-6 px-8 w-1/2'
             },
         },
     }
@@ -101,7 +120,7 @@
     }
 
     .nova-grid-field-label {
-        font-weight: 700;
+        // font-weight: 700;
         margin-bottom: 0.5rem;
         width: 100%;
     }
